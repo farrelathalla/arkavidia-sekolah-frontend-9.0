60fbb5ca3e56adb970aee7736dfaf19345aa6d02
farrelathalla <farrelxag@gmail.com>
2024-11-30 21:43:52 +0700

    feat: add data

---
+++ src/data/factData.ts
@@ -0,0 +1,27 @@
+export interface Fact {
+  title: string;
+  description: string;
+}
+
+export const facts: Fact[] = [
+  {
+    title: "COMPETITION",
+    description:
+      "Competition merupakan salah satu main event yang diselenggarakan untuk menguji kemampuan peserta dalam memecahkan persoalan yang ada pada berbagai bidang teknologi informasi. Dengan adanya perlombaan ini, diharapkan peserta pada khususnya dapat melatih kemampuan pemecahan masalah yang ada sebagai bentuk persiapan dalam memecahkan masalah-masalah yang ditemukan di dunia nyata. Terdapat empat cabang perlombaan yang diselenggarakan dalam Arkavidia 8.0, yakni Competitive Programming, Datavidia, UXvidia, dan Arkalogica.",
+  },
+  {
+    title: "COMPETITION",
+    description:
+      "Competition merupakan salah satu main event yang diselenggarakan untuk menguji kemampuan peserta dalam memecahkan persoalan yang ada pada berbagai bidang teknologi informasi. Dengan adanya perlombaan ini, diharapkan peserta pada khususnya dapat melatih kemampuan pemecahan masalah yang ada sebagai bentuk persiapan dalam memecahkan masalah-masalah yang ditemukan di dunia nyata. Terdapat empat cabang perlombaan yang diselenggarakan dalam Arkavidia 8.0, yakni Competitive Programming, Datavidia, UXvidia, dan Arkalogica.",
+  },
+  {
+    title: "COMPETITION",
+    description:
+      "Competition merupakan salah satu main event yang diselenggarakan untuk menguji kemampuan peserta dalam memecahkan persoalan yang ada pada berbagai bidang teknologi informasi. Dengan adanya perlombaan ini, diharapkan peserta pada khususnya dapat melatih kemampuan pemecahan masalah yang ada sebagai bentuk persiapan dalam memecahkan masalah-masalah yang ditemukan di dunia nyata. Terdapat empat cabang perlombaan yang diselenggarakan dalam Arkavidia 8.0, yakni Competitive Programming, Datavidia, UXvidia, dan Arkalogica.",
+  },
+  {
+    title: "COMPETITION",
+    description:
+      "Competition merupakan salah satu main event yang diselenggarakan untuk menguji kemampuan peserta dalam memecahkan persoalan yang ada pada berbagai bidang teknologi informasi. Dengan adanya perlombaan ini, diharapkan peserta pada khususnya dapat melatih kemampuan pemecahan masalah yang ada sebagai bentuk persiapan dalam memecahkan masalah-masalah yang ditemukan di dunia nyata. Terdapat empat cabang perlombaan yang diselenggarakan dalam Arkavidia 8.0, yakni Competitive Programming, Datavidia, UXvidia, dan Arkalogica.",
+  },
+];
---
+++ src/data/faqData.ts
@@ -0,0 +1,27 @@
+export interface FAQ {
+  question: string;
+  answer: string;
+}
+
+export const faqData: FAQ[] = [
+  {
+    question: "Apa itu Arkavidia 8.0?",
+    answer:
+      "Arkavidia 8.0 merupakan acara dan kompetisi IT tahunan yang diadakan oleh Himpunan Mahasiswa Informatika (HMIF) ITB. Dengan mengusung tema “Adaptive Collaboration to Encounter Digital Disruption”, Arkavidia 8.0 diharapkan dapat lebih banyak menyadarkan individu akan hak dan kewajibannya di dunia digital, sehingga dapat mewujudkan kemerdekaan digital bagi dirinya. Diharapkan Arkavidia 8.0 dapat memberikan manfaat bagi pelajar, civitas akademik, dan masyarakat umum seluruh Indonesia.",
+  },
+  {
+    question: "Apa itu Arkavidia 8.0?",
+    answer:
+      "Arkavidia 8.0 merupakan acara dan kompetisi IT tahunan yang diadakan oleh Himpunan Mahasiswa Informatika (HMIF) ITB. Dengan mengusung tema “Adaptive Collaboration to Encounter Digital Disruption”, Arkavidia 8.0 diharapkan dapat lebih banyak menyadarkan individu akan hak dan kewajibannya di dunia digital, sehingga dapat mewujudkan kemerdekaan digital bagi dirinya. Diharapkan Arkavidia 8.0 dapat memberikan manfaat bagi pelajar, civitas akademik, dan masyarakat umum seluruh Indonesia.",
+  },
+  {
+    question: "Apa itu Arkavidia 8.0?",
+    answer:
+      "Arkavidia 8.0 merupakan acara dan kompetisi IT tahunan yang diadakan oleh Himpunan Mahasiswa Informatika (HMIF) ITB. Dengan mengusung tema “Adaptive Collaboration to Encounter Digital Disruption”, Arkavidia 8.0 diharapkan dapat lebih banyak menyadarkan individu akan hak dan kewajibannya di dunia digital, sehingga dapat mewujudkan kemerdekaan digital bagi dirinya. Diharapkan Arkavidia 8.0 dapat memberikan manfaat bagi pelajar, civitas akademik, dan masyarakat umum seluruh Indonesia.",
+  },
+  {
+    question: "Apa itu Arkavidia 8.0?",
+    answer:
+      "Arkavidia 8.0 merupakan acara dan kompetisi IT tahunan yang diadakan oleh Himpunan Mahasiswa Informatika (HMIF) ITB. Dengan mengusung tema “Adaptive Collaboration to Encounter Digital Disruption”, Arkavidia 8.0 diharapkan dapat lebih banyak menyadarkan individu akan hak dan kewajibannya di dunia digital, sehingga dapat mewujudkan kemerdekaan digital bagi dirinya. Diharapkan Arkavidia 8.0 dapat memberikan manfaat bagi pelajar, civitas akademik, dan masyarakat umum seluruh Indonesia.",
+  },
+];
