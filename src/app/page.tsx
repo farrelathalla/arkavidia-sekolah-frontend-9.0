92bf4a961c45a372c2f29a61a5d40ac1eaa21538
farrelathalla <farrelxag@gmail.com>
2024-11-30 22:11:09 +0700

    feat: add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-    return <div>Home</div>;
-}
+"use client";
+import Hero from "../components/Hero";
+import CountdownClose from "../components/CountdownClose";
+import WhatIs from "../components/WhatIs";
+import Timeline from "../components/Timeline";
+import Faq from "../components/Faq";
+import Footer from "../components/Footer";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
+import AOS from "aos";
+
+const page = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <main className="overflow-x-hidden">
+      <Hero />
+      <CountdownClose />
+      <WhatIs />
+      <Timeline />
+      <Faq />
+      <Footer />
+    </main>
+  );
+};
+
+export default page;
